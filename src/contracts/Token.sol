aeb5d6fc64edeca228b7c0c291da8d0098d09534
Orian Zinger <zinger777@gmail.com>
2021-05-06 00:40:07 +0300

    Contract created

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,21 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity 0.7.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+    address public minter;
 
-  //add minter changed event
+    event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    constructor() ERC20("ERC20 Name", "ZZZ") payable {
+        minter = msg.sender;
+    }
 
-  //Add pass minter role function
+    function passMinterRole(address bank) public returns (bool) {
+        require(msg.sender == minter, "Failed to pass the minter role since only the owner can pass it");
+        minter = bank;
+        emit MinterChanged(msg.sender, bank);
+        return true;
+    }
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
-	}
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == minter, string(abi.encodePacked("Failed to mint for sender:", msg.sender, " since they are not the minter")));
+        _mint(account, amount);
+    }
 }
