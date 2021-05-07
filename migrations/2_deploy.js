93532a25715da24ca2008cf20e87d6259fe486fb
Orian Zinger <zinger777@gmail.com>
2021-05-07 18:51:33 +0300

    Simple deployment added

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,7 +2,7 @@ const Token = artifacts.require("Token");
 const dBank = artifacts.require("dBank");
 
 module.exports = async function(deployer) {
-	//deploy Token
+	await deployer.deploy(Token);
 
 	//assign token into variable to get it's address
 	
